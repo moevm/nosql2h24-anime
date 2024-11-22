40b164fd725e6cffde4a976e59f8cd989441d48a
Александр Петров <alexs_petrov_2003@mail.ru>
2024-11-22 23:37:48 +0300

    Pagination
    Добавлены кнопки для пагинации

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,13 +1,33 @@
 import React from 'react';
 
-const Footer = () => {
+/*const Footer = () => {
     return (
         <form className="footer">
             <button>Назад</button>
-            1-2-3-----------------------------------------------------------------98-99
+            1-2-3-----------------------------------------------------------------98-99-100
             <button>Вперед </button>
         </form>
     );
-};
+};*/
+
+const Footer = ({ currentPage, totalPages, onPageChange }) => (
+    <div className="footer">
+        <button onClick={() => onPageChange(currentPage - 1)} disabled={currentPage === 1}>
+            Назад
+        </button>
+        {Array.from({ length: totalPages }, (_, index) => index + 1).map((page) => (
+            <button
+                key={page}
+                onClick={() => onPageChange(page)}
+                className={currentPage === page ? 'active' : ''}
+            >
+                {page}
+            </button>
+        ))}
+        <button onClick={() => onPageChange(currentPage + 1)} disabled={currentPage === totalPages}>
+            Вперед
+        </button>
+    </div>
+);
 
 export default Footer;
